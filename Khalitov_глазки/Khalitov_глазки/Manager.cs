278a683913be8c5372edd50cd8e27f082fc38190
Arsen Khalitov <halitovarsen394@gmail.com>
2023-11-03 10:45:50 +0400

    10 лабораторная каркас

---
+++ Khalitov_глазки/Khalitov_глазки/Manager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Khalitov_глазки
+{
+    class Manager
+    {
+        public static Frame MainFrame { get; set; }
+    }
+}
